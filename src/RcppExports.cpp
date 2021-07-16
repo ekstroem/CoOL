310a580be5273d6e11d807c63a39d5d26a8c30e2
Andreas Rieckmann <44702127+AndreasRieckmann@users.noreply.github.com>
2021-07-16 15:52:16 +0200

    Version nr

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcpprelu
 arma::mat rcpprelu(const arma::mat& x);
 RcppExport SEXP _CoOL_rcpprelu(SEXP xSEXP) {
